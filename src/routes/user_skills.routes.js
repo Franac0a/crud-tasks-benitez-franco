dd81bedebdda0b9847a9b4de1a66a9625e272a23
Franac0a <fbenitez0021@gmail.com>
2025-08-27 21:56:40 -0300

    correcciones

--- src/routes/user_skills.routes.js
+++ src/routes/user_skills.routes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 import {
   getAllSkills,
@@ -9,15 +8,3 @@ export const routerSkill = express.Router();
 
 routerSkill.get("/skills", getAllSkills);
 routerSkill.post("/skills", createSkill);
-=======
-import { Router } from "express";
-import {
-  createSkill,
-  getAllSkills,
-} from "../controllers/user_skills_controllers.js";
-
-export const routerSkills = Router();
-
-routerSkills.post("/skills", createSkill);
-routerSkills.get("/skills", getAllSkills);
->>>>>>> 3676b0298ac13da33e1dd12ad20a5f45c0ab0a93
